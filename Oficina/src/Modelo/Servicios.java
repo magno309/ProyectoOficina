51e95eace96c57c1eaa6cb6ed0d779e564fa7266
Luis Abraham Sanabria Melecio <luis_abraham98@outlook.com>
2020-07-13 14:41:20 -0500

    Creacion de clases

---
+++ Oficina/src/Modelo/Servicios.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo;
+
+/**
+ *
+ * @author luis_
+ */
+public class Servicios {
+    private int Id;
+    private String Nombre;
+    private String Direccion;
+    private String Telefono;
+    private String Celular;
+
+    public Servicios(int Id, String Nombre) {
+        this.Id = Id;
+        this.Nombre = Nombre;
+    }
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int Id) {
+        this.Id = Id;
+    }
+    
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    public String getDireccion() {
+        return Direccion;
+    }
+
+    public void setDireccion(String Direccion) {
+        this.Direccion = Direccion;
+    }
+
+    public String getTelefono() {
+        return Telefono;
+    }
+
+    public void setTelefono(String Telefono) {
+        this.Telefono = Telefono;
+    }
+
+    public String getCelular() {
+        return Celular;
+    }
+
+    public void setCelular(String Celular) {
+        this.Celular = Celular;
+    }
+}
